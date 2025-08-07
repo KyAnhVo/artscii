c9605077fb395f1dffabb4448521c09e25df925e
Ky Anh Vo <tommyvo0406@gmail.com>
2025-08-07 09:44:27 -0500

    Png::Png finished Png header 8 bytes check

--- src/png_decoder.cpp
+++ src/png_decoder.cpp
@@ -2,23 +2,28 @@
 
 #include <string>
 #include <stdio.h>
+#include <stdexcept>
 
 const uint8_t PNG_HEADER[8] = {0x89, 0x50, 0x4E, 0x47,0x0D, 0x0A, 0x1A, 0x0A};
 
 Png::Png(std::string file_name) {
     FILE * file = fopen(file_name.c_str(), "r");
 
-    
     // Confirm file is actually a PNG:
 
-    while (file) {
-        uint8_t curr_ind = 0;
-        uint8_t curr_char = fgetc(file);
-        
-        if (PNG_HEADER[curr_ind] != curr_char) {
-            throw 1;
+    uint8_t byte;
+    for (uint8_t ind = 0; ind < 8; ind++) {
+        size_t read_bytes = fread(&byte, 1, 1, file);
+        if (read_bytes != 1) {
+            throw new std::runtime_error("image reading failed");
         }
-
-        curr_ind ++;
+        if (byte != PNG_HEADER[ind]) {
+            throw new std::runtime_error("image is either not png or corrupted");
+        }
     }
+
+    // Read IHDR chunk (IHDR = image header)
+    //
+
+
 }
